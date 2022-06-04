173b30d3b9ddb6e1172cdb0535fd9aaf356cf19a
ERUIHNIYHBKBNF <839203521@qq.com>
2022-06-04 15:03:31 +0800

    fix: 取消房源详情接口鉴权

--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,39 @@ import bodyParser from "koa-body";
 import "reflect-metadata";
 import logger from "./utils/logger";
 import router from "./router";
-import config from './config';
-import dataSource from './db';
-import koajwt from 'koa-jwt'
+import config from "./config";
+import dataSource from "./db";
+import koajwt from "koa-jwt";
 const { APP_PORT, JWT_SECRET } = config;
 
-dataSource.initialize().then(() => {
-  const app = new Koa();
+dataSource
+  .initialize()
+  .then(() => {
+    const app = new Koa();
 
-  app.use(logger());
-  app.use(cors());
-  app.use(bodyParser());
-  app.use(koajwt({
-    secret: JWT_SECRET + ''
-  }).unless({ // 配置白名单
-    path: [/\/user\/register/, /\/user\/login/,/\/user\/verify/]
-  }))
-  app.use(router.routes()).use(router.allowedMethods());
+    app.use(logger());
+    app.use(cors());
+    app.use(bodyParser());
+    app.use(
+      koajwt({
+        secret: JWT_SECRET + "",
+      }).unless({
+        // 配置白名单
+        path: [
+          /\/user\/register/,
+          /\/user\/login/,
+          /\/user\/verify/,
+          /\/home\/list/,
+          /\/home\//,
+        ],
+      })
+    );
+    app.use(router.routes()).use(router.allowedMethods());
 
-  app.listen(APP_PORT, () => {
-    console.log(`Server listening on port ${APP_PORT}`);
+    app.listen(APP_PORT, () => {
+      console.log(`Server listening on port ${APP_PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
   });
-}).catch(err => {
-  console.error(err);
-});
